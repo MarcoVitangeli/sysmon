ed76c6ee4d34e50e6fcc4e1982e43629e57fae37
Marco Vitangeli <marco_augusto_vitangeli@outlook.com>
2025-07-23 23:25:25 -0300

    fix bug in supervisor

--- main.exs
+++ main.exs
@@ -13,6 +13,7 @@ we need to find the files to monitor in:
 
 defmodule Sysmon.ContainerMonitorSupervisor do
   use Supervisor
+  require Logger
 
   @cgroup_dir "/sys/fs/cgroup/system.slice/"
   @docker_dir_regex ~r/^docker-([a-f0-9]{64})\.scope$/
@@ -44,8 +45,9 @@ defmodule Sysmon.ContainerMonitorSupervisor do
         end
     end)
     |> Enum.map(fn {_, pid, _, _} -> pid end)
-    |> Enum.map(fn pid -> :timer.send_interval(500, pid, :emit) end)
+    |> Enum.map(fn pid -> :timer.send_interval(2000, pid, :emit) end)
     |> Enum.map(fn {:ok, handle} -> handle end)
+
     {:ok, handles}
   end
 
@@ -63,6 +65,7 @@ end
 
 defmodule Sysmon.ProcessMonitor do
   use GenServer
+  require Logger
 
   def start_link(cgroup_dir) when is_binary(cgroup_dir) do
     GenServer.start_link(__MODULE__, cgroup_dir, name: __MODULE__)
@@ -82,11 +85,10 @@ defmodule Sysmon.ProcessMonitor do
         end
       end)
       |> Enum.map(fn {{:sysmon_cpu_worker, dir}, _, :worker, [Sysmon.ContainerMonitor]} -> dir end)
-      |> MapSet.new
 
     case File.ls(cgroup_dir) do
       {:ok, entries} -> check_entries(cgroup_dir, entries, existing_dirs)
-      {:error, _} -> IO.puts("ERROR reading cgroup file")
+      {:error, _} -> Logger.info("ERROR reading cgroup file")
     end
 
     Process.send_after(self(), :monitor, 1500)
@@ -94,33 +96,45 @@ defmodule Sysmon.ProcessMonitor do
   end
 
   def check_entries(cgroup_dir, entries, existing_dirs) do
-    IO.puts("CHECKING_ENTRIES")
-    #entries
-    #|> Enum.filter(fn dir ->  Regex.match?(@docker_dir_regex, dir) end)
-    #|> Enum.map(fn dir -> Path.join(cgroup_dir, dir) end)
-    #|> Enum.filter(fn dir -> !MapSet.member?(existing_dirs, dir) end)
-    #|> Enum.map(&add_monitor/1)
-    IO.puts("END_CHECK")
+    Logger.info("Start child check removal")
+    entry_set = entries |> MapSet.new
+
+    existing_dirs 
+    |> Enum.filter(fn dir -> !MapSet.member?(entry_set, dir) end)
+    |> Enum.map(fn dir -> remove_non_existent(dir) end)
+    Logger.info("End child check removal")
+  end
+
+  def remove_non_existent(dir) do
+    Logger.info("Deleting child " <> dir)
+    :ok = Supervisor.terminate_child(Sysmon.ContainerMonitorSupervisor, {:sysmon_cpu_worker, dir})
+    :ok = Supervisor.delete_child(Sysmon.ContainerMonitorSupervisor, {:sysmon_cpu_worker, dir})
   end
 end
 
 defmodule Sysmon.ContainerMonitor do
   use GenServer
+  require Logger
 
   def start_link(path) when is_binary(path) do
     GenServer.start_link(__MODULE__, path)
   end
 
   def init(path) when is_binary(path) do
+    Logger.info("STARTING TO MONITOR " <> path)
     {:ok, path} # maybe more state to come
   end
 
   def handle_info(:emit, container_path) do
-    IO.puts("START_EMIT for dir " <> container_path)
-    
-    get_cpu_metrics(container_path) |> Enum.map(fn e -> IO.inspect(e) end)
+    Logger.info("START_EMIT for dir " <> container_path)
 
-    IO.puts("END_EMIT for dir " <> container_path)
+    content = get_cpu_metrics(container_path)
+      |> Enum.map(fn %{:name => n, :value => v, :source => _, :type => _} -> "Name: " <> n <> " Value #{v}" end)
+      |> Enum.reduce("", fn x, acc -> x <> "\n" <> acc end)
+
+    File.write("./data#{DateTime.utc_now() |> DateTime.to_unix(:millisecond)}.txt", content, [:write])
+
+    Logger.info("END_EMIT for dir " <> container_path)
 
     {:noreply, container_path}
   end
@@ -146,9 +160,7 @@ defmodule Sysmon.Main do
     {:ok, _} = Sysmon.ContainerMonitorSupervisor.start_link()
     {:ok, handles} = Sysmon.ContainerMonitorSupervisor.start_metric_emit()
 
-    Process.sleep(16000)
-  
-    Sysmon.ContainerMonitorSupervisor.cancel_metric_emit(handles)
+    Process.sleep(:infinity)
   end
 end
 
